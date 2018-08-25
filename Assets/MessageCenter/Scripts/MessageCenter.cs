3ce18366c34bd4f27c5bb778c2605f3346250447
DESKTOP-LKON452\catze <catzeromeio@hotmail.com>
2018-08-26 00:47:14 +0800

    init

---
+++ Assets/MessageCenter/Scripts/MessageCenter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CZM
+{
+    public struct Message
+    {
+        public object Sender;
+        public object Receiver;
+        public object Data;
+    }
+
+    public delegate void MessageDelegate(Message mes);
+    public class MessageCenter
+    {
+        static void AddListener()
+        {
+        }
+
+        static void RemoveListener(Message mes)
+        {
+
+        }
+
+        static void RemoveAllListener()
+        {
+
+        }
+
+        //immedate, next frame, some seconds
+        static void SendEvent()
+        {
+
+        }
+
+        public static void Update()
+        {
+
+        }
+    }
+}
+
