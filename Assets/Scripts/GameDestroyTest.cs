9253a543c37d580cb93d1a16813fbf75f5497b3e
DESKTOP-LKON452\catze <catzeromeio@hotmail.com>
2018-08-26 00:48:59 +0800

    测试委托中访问，被Destroy的GameObject对象

---
+++ Assets/Scripts/GameDestroyTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDestroyTest : MonoBehaviour
+{
+    public delegate int GetInstanceIDDel();
+
+    public GameObject TestGO;
+
+    GetInstanceIDDel GetTestInstanceID;
+
+    void Start()
+    {
+        Debug.Log("the test gomeobject instanceID is " + TestGO.GetInstanceID());
+        GetTestInstanceID = TestGO.GetInstanceID;
+        Destroy(TestGO);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATE the test gomeobject instanceID is " + GetTestInstanceID());
+    }
+}
